bc424fa17451629c4e8c90c2b3c113ce012dcd8a
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-11-07 12:11:47 +0530

    access token and refresh token, implemented refresh token for front-end route

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
@@ -10,8 +10,8 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
       req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
+      throw new ApiError(401, "Unauthorized request");
     }
-    throw new ApiError(401, "Unauthorized request");
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import {
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
 } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multar.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multar.middleware.js";
 
 const router = Router();
 
@@ -23,5 +24,6 @@ router.route("/login").post(loginUser);
 
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
